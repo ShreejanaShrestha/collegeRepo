b4b40a68ead3b1b9b662efcdb698782890eadc56
Shreejana Shrestha <stha.shreezana@gmail.com>
2025-02-18 21:36:22 -0500

    Adding java materials and assignment Term 3

---
+++ Term3/Java/Assignments/Assignment1/GroupAAssignment1/src/main/java/GroupA/Login.java
@@ -0,0 +1,54 @@
+package GroupA;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (username.isEmpty() || password.isEmpty()) {
+			RequestDispatcher req = request.getRequestDispatcher("Register3.jsp");
+			req.include(request, response);
+		} else {
+			RequestDispatcher req = request.getRequestDispatcher("Register4.jsp");
+			req.forward(request, response);
+		}
+	}
+
+}
